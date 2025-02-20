a655630063aca9449fa5a6ba1960a2fe8fa131d6
@a.malkov_automacon.ru <a.malkov@automacon.ru>
2025-02-20 19:59:42 +0700

    test resp

--- src/app/api/users/profile/route.ts
+++ src/app/api/users/profile/route.ts
@@ -16,7 +16,21 @@ export async function GET(req: NextRequest) {
 
         const response = await serverReq.json();
 
-        const res = NextResponse.json(response);
+        // const res = NextResponse.json(response);
+
+        const res = NextResponse.json({
+            "id": 56,
+            "email": "malkov.fighter@yandex.ru",
+            "password": "$argon2id$v=19$m=65536,t=3,p=4$6WefCc7c1wuV/ZiehP+Ozw$5+UndhFrv/ITiEkUwCFz8HYhiL42ZtoRr60Aru54xXQ",
+            "display_name": "Alex",
+            "picture": "",
+            "role": "REGULAR",
+            "is_verified": true,
+            "is_two_factor_enabled": false,
+            "method": "CREDENTIALS",
+            "created_at": "2025-02-19T09:41:06.844Z",
+            "updated_at": "2025-02-19T09:41:06.844Z"
+        });
 
         return res;
 
